294bb88bdcd29454ff07659157b7396377959ee8
ruchitavishnoi <vishnoiruchita@gmail.com>
2026-03-11 15:18:37 +0530

    greet user from command line
    Refactored main method to accept command line argument for name.

--- Hello App.java
+++ Hello App.java
@@ -1,13 +1,5 @@
-import java.util.Scanner;
-
 public class Hello App {
-  public static void main(String[] args) {
-    
-    Scanner scanner = new Scanner(Systrem.in);
-    System.out.print("Please enter your name: ");
-    String name = scanner.nextline();
-    System.out.println("Hello, " + name + "!");
-    
-    scanner.close();
-  }
+    public static void main(String[] args) {
+        System.out.println("Hello, " + args[0] + "! Welcome!");
+    }
 }
