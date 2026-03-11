ae276ebd840c4b858a7b4ca9e5678d7b84391ff8
ruchitavishnoi <vishnoiruchita@gmail.com>
2026-03-11 15:22:30 +0530

    "Hello" with Command-Line Argument

---
+++ Hello App.java
@@ -0,0 +1,6 @@
+public class Hello App {
+    public static void main(String[] args) {
+        String name = (args.length > 0) ? args[0] : "User";
+        System.out.println("Hello " + name);
+    }
+}4
