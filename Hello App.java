58c5c0a586e61131026a0f88711bbea82ef89d83
ruchitavishnoi <vishnoiruchita@gmail.com>
2026-03-17 12:53:52 +0530

    diaplay hello with name using enhanced loops

---
+++ Hello App.java
@@ -0,0 +1,19 @@
+public class Hello App {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
+        }
+        StringBuilder nameBuilder = new StringBuilder();
+        boolean first = true;
+
+        for (String name : args) {
+            if (!first) {
+                nameBuilder.append(", ");
+            }
+            nameBuilder.append(name);
+            first = false;
+        }
+        System.out.println("Hello, " + nameBuilder.toString() + "!");
+    }
+}
